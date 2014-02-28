8e0646804e0e71d33f6f0ea3900130fea0fb9d28
Zhelyazko Zhelev <zhelyazko.zhelev@scalefocus.com>
2014-02-28 18:26:01 +0200

    Removed unnecessary code.

--- src/com/aviq/tv/android/sdk/feature/language/FeatureLanguage.java
+++ src/com/aviq/tv/android/sdk/feature/language/FeatureLanguage.java
@@ -10,13 +10,10 @@
 
 package com.aviq.tv.android.sdk.feature.language;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.Locale;
 
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.util.Log;
 
 import com.aviq.tv.android.sdk.core.Environment;
 import com.aviq.tv.android.sdk.core.Prefs;
@@ -113,42 +110,5 @@ public class FeatureLanguage extends FeatureComponent
 		config.locale = locale;
 		Resources res = Environment.getInstance().getResources();
 		res.updateConfiguration(config, res.getDisplayMetrics());
-	}
-
-	private void setSystemLanguage2(Locale locale)
-	{
-		try
-		{
-			Class<?> amnClass = Class.forName("android.app.ActivityManagerNative");
-			Object amn = null;
-			Configuration config = null;
-
-			// amn = ActivityManagerNative.getDefault();
-			Method methodGetDefault = amnClass.getMethod("getDefault");
-			methodGetDefault.setAccessible(true);
-			amn = methodGetDefault.invoke(amnClass);
-
-			// config = amn.getConfiguration();
-			Method methodGetConfiguration = amnClass.getMethod("getConfiguration");
-			methodGetConfiguration.setAccessible(true);
-			config = (Configuration) methodGetConfiguration.invoke(amn);
-
-			// config.userSetLocale = true;
-			Class<? extends Configuration> configClass = config.getClass();
-			Field f = configClass.getField("userSetLocale");
-			f.setBoolean(config, true);
-
-			// set the locale to the new value
-			config.locale = locale;
-
-			// amn.updateConfiguration(config);
-			Method methodUpdateConfiguration = amnClass.getMethod("updateConfiguration", Configuration.class);
-			methodUpdateConfiguration.setAccessible(true);
-			methodUpdateConfiguration.invoke(amn, config);
-		}
-		catch (Exception e)
-		{
-			Log.e(TAG, "Error changing locale: " + e.getMessage(), e);
-		}
 	}
 }
