3e36f8a3e9eada913d86a03a35e2583cce84331b
Alexander Marinov <alek.marinov@aviq.com>
2014-03-05 19:51:25 +0200

    comment change

--- src/com/aviq/tv/android/sdk/feature/epg/FeatureEPG.java
+++ src/com/aviq/tv/android/sdk/feature/epg/FeatureEPG.java
@@ -295,6 +295,7 @@ public abstract class FeatureEPG extends FeatureScheduler
 		@Override
 		public void onErrorResponse(VolleyError error)
 		{
+			// FIXME: This error occurs when the EPG server is down. Show appropriate message
 			int statusCode = error.networkResponse != null ? error.networkResponse.statusCode
 			        : ResultCode.GENERAL_FAILURE;
 			Log.e(TAG, "Error retrieving channels with code " + statusCode + ": " + error);
