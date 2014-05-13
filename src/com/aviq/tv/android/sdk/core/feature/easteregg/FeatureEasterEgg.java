cece734f87984f217908a8e8551ccd2a732d6b87
Zhelyazko Zhelev <zhelyazko.zhelev@scalefocus.com>
2014-05-13 12:33:40 +0300

    Refactored EasterEgg feature to support an arbitrary number of key combinations. Supported keys: Yellow, Red, Green, Blue, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9

--- src/com/aviq/tv/android/sdk/core/feature/easteregg/FeatureEasterEgg.java
+++ src/com/aviq/tv/android/sdk/core/feature/easteregg/FeatureEasterEgg.java
@@ -10,6 +10,12 @@
 
 package com.aviq.tv.android.sdk.core.feature.easteregg;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.os.Bundle;
 import android.util.Log;
 
@@ -17,6 +23,7 @@ import com.aviq.tv.android.sdk.core.Environment;
 import com.aviq.tv.android.sdk.core.EventMessenger;
 import com.aviq.tv.android.sdk.core.EventReceiver;
 import com.aviq.tv.android.sdk.core.Key;
+import com.aviq.tv.android.sdk.core.ResultCode;
 import com.aviq.tv.android.sdk.core.feature.FeatureComponent;
 import com.aviq.tv.android.sdk.core.feature.FeatureName;
 import com.aviq.tv.android.sdk.core.feature.FeatureName.Component;
@@ -28,8 +35,14 @@ import com.aviq.tv.android.sdk.utils.TextUtils;
 public class FeatureEasterEgg extends FeatureComponent implements EventReceiver
 {
 	public static final String TAG = FeatureEasterEgg.class.getSimpleName();
+
+	public static int ON_KEY_SEQUENCE = EventMessenger.ID("ON_KEY_SEQUENCE");
+	public static final String EXTRA_KEY_SEQUENCE = "KEY_SEQUENCE";
+
 	private long lastKeyPress = 0;
 	private StringBuffer _sequence = new StringBuffer();
+	private List<String> _sequenceList = new ArrayList<String>();
+	private Map<String, String> _globalSequenceMap = new HashMap<String, String>();
 
 	public enum Param
 	{
@@ -39,14 +52,19 @@ public class FeatureEasterEgg extends FeatureComponent implements EventReceiver
 		MIN_KEY_DELAY(1000),
 
 		/**
-		 * The expected key sequence for the easter egg
+		 * The expected comma-separated key sequences for the easter egg.
 		 */
-		KEY_SEQUENCE("YGRB"),
+		KEY_SEQUENCES(""),
+
+		/**
+		 * Key sequence global to the application using this feature.
+		 */
+		KEY_SEQUENCE_YGRB("YGRB"),
 
 		/**
 		 * The expected app package to start
 		 */
-		START_PACKAGE("com.android.settings");
+		KEY_SEQUENCE_ACTION_YGRB("com.android.settings");
 
 		Param(int value)
 		{
@@ -62,6 +80,29 @@ public class FeatureEasterEgg extends FeatureComponent implements EventReceiver
 	public FeatureEasterEgg()
 	{
 		Environment.getInstance().getEventMessenger().register(this, Environment.ON_KEY_PRESSED);
+	}
+
+	@Override
+	public void initialize(final OnFeatureInitialized onFeatureInitialized)
+	{
+		Log.i(TAG, ".initialize");
+
+		// A list of the application-specific key sequences
+
+		String sequences = getPrefs().getString(Param.KEY_SEQUENCES);
+		String[] seqArray = sequences.split(",");
+		_sequenceList = Arrays.asList(seqArray);
+
+		// Process global key sequence mapping
+
+		String keySeq = null;
+		String keySeqAction = null;
+
+		keySeq = getPrefs().getString(Param.KEY_SEQUENCE_YGRB);
+		keySeqAction = getPrefs().getString(Param.KEY_SEQUENCE_ACTION_YGRB);
+		_globalSequenceMap.put(keySeq, keySeqAction);
+
+		onFeatureInitialized.onInitialized(this, ResultCode.OK);
 	}
 
 	@Override
@@ -86,10 +127,19 @@ public class FeatureEasterEgg extends FeatureComponent implements EventReceiver
 			if (chr != '\0')
 			{
 				_sequence.append(chr);
-				if (_sequence.toString().equals(getPrefs().getString(Param.KEY_SEQUENCE)))
+				String keySeq = _sequence.toString();
+
+				if (_globalSequenceMap.containsKey(keySeq))
 				{
-					Environment.getInstance().startAppPackage(getPrefs().getString(Param.START_PACKAGE));
+					Environment.getInstance().startAppPackage(_globalSequenceMap.get(keySeq));
 				}
+				else if (_sequenceList.contains(keySeq))
+				{
+					Bundle params = new Bundle();
+					params.putString(EXTRA_KEY_SEQUENCE, keySeq);
+					getEventMessenger().trigger(ON_KEY_SEQUENCE, params);
+				}
+
 				lastKeyPress = System.currentTimeMillis();
 			}
 		}
@@ -107,6 +157,26 @@ public class FeatureEasterEgg extends FeatureComponent implements EventReceiver
 				return 'Y';
 			case BLUE:
 				return 'B';
+			case NUM_0:
+				return '0';
+			case NUM_1:
+				return '1';
+			case NUM_2:
+				return '2';
+			case NUM_3:
+				return '3';
+			case NUM_4:
+				return '4';
+			case NUM_5:
+				return '5';
+			case NUM_6:
+				return '6';
+			case NUM_7:
+				return '7';
+			case NUM_8:
+				return '8';
+			case NUM_9:
+				return '9';
 			default:
 				return '\0';
 		}
