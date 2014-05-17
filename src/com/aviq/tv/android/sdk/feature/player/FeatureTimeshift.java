00356f2c4ede8f7bef9c46947711d9754fa947f7
Alexander Marinov <alek.marinov@aviq.com>
2014-05-17 13:28:17 +0300

    timeshift component adapter to new timeshift proxy

--- src/com/aviq/tv/android/sdk/feature/player/FeatureTimeshift.java
+++ src/com/aviq/tv/android/sdk/feature/player/FeatureTimeshift.java
@@ -10,11 +10,6 @@
 
 package com.aviq.tv.android.sdk.feature.player;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import android.os.Bundle;
 
 import com.aviq.tv.android.sdk.core.Environment;
@@ -25,7 +20,6 @@ import com.aviq.tv.android.sdk.core.feature.FeatureComponent;
 import com.aviq.tv.android.sdk.core.feature.FeatureName;
 import com.aviq.tv.android.sdk.core.feature.FeatureName.Component;
 import com.aviq.tv.android.sdk.core.feature.FeatureNotFoundException;
-import com.aviq.tv.android.sdk.feature.system.NetworkClient;
 import com.aviq.tv.android.sdk.utils.TextUtils;
 
 /**
@@ -45,90 +39,26 @@ public class FeatureTimeshift extends FeatureComponent implements EventReceiver
 	private long _playTimeDelta;
 	private int _timeshiftMaxBufSize;
 	private AutoResumer _autoResumer = new AutoResumer();
-	private String _timeshiftUrl;
-	private String _timeshiftStartUrl;
-	private DateFormat _timeshiftTimeFormat;
-	private NetworkClient _networkClient;
-	private boolean _timeshiftAvailable = false;
+	private String _streamUrl;
 
 	private Runnable _seeker = new Runnable()
 	{
 		@Override
 		public void run()
 		{
-			String timeshiftTime = _timeshiftTimeFormat.format(new Date(1000 * getPlayingTime()));
-			Bundle bundle = new Bundle();
-			bundle.putString("TIME", timeshiftTime);
-			String seekUrl = _timeshiftUrl + getPrefs().getString(Param.TIMESHIFT_SEEK_URL_PARAM, bundle);
+			String seekUrl = _streamUrl + "&timeshift=" + _playTimeDelta;
 			_feature.Component.PLAYER.play(seekUrl);
 		}
 	};
-
-//	private Runnable _switcher = new Runnable()
-//	{
-//		@Override
-//		public void run()
-//		{
-//			int status = ffmpeg_running();
-//			Log.d(TAG, "waiting ffmpeg for status playing (" + FF_PLAYING + "): current is " + status);
-//			if (status == FF_PLAYING)
-//			{
-//				_featurePlayer.play(_timeshiftStartUrl);
-//			}
-//			else
-//			{
-//				if (status != FF_IDLE)
-//				{
-//					getEventMessenger().postDelayed(this, 100);
-//				}
-//			}
-//		}
-//	};
 
 	public enum Param
 	{
 		/**
 		 * Timeshift max buffer size in seconds
 		 */
-		TIMESHIFT_DURATION(60 * 60),
-
-		/**
-		 * timeshift base url
-		 */
-		TIMESHIFT_URL("http://localhost:8090/live.ts"),
-
-		/**
-		 * Start timeshift param
-		 */
-		TIMESHIFT_START_URL_PARAM(""), // ?buffer=1
-
-		/**
-		 * Seek in timeshift param
-		 */
-		TIMESHIFT_SEEK_URL_PARAM("?date=${TIME}"),
-
-		/**
-		 * Timeshift time format
-		 */
-		// TIME_FORMAT("yyyy-MM-dd'T'HH:mm:ss")
-		TIME_FORMAT("HH:mm:ss.000"),
-
-		/**
-		 * Timeshift server port number
-		 */
-		PORT(7070),
-
-		/**
-		 * Timeshift host address
-		 */
-		HOST("127.0.0.1");
+		TIMESHIFT_DURATION(60 * 60);
 
 		Param(int value)
-		{
-			Environment.getInstance().getFeaturePrefs(FeatureName.Component.TIMESHIFT).put(name(), value);
-		}
-
-		Param(String value)
 		{
 			Environment.getInstance().getFeaturePrefs(FeatureName.Component.TIMESHIFT).put(name(), value);
 		}
@@ -137,7 +67,6 @@ public class FeatureTimeshift extends FeatureComponent implements EventReceiver
 	public FeatureTimeshift() throws FeatureNotFoundException
 	{
 		require(FeatureName.Component.PLAYER);
-		require(FeatureName.Component.SYSTEM);
 	}
 
 	@Override
@@ -148,96 +77,10 @@ public class FeatureTimeshift extends FeatureComponent implements EventReceiver
 		_feature.Component.PLAYER.getEventMessenger().register(this, FeaturePlayer.ON_PLAY_PAUSE);
 		_timeshiftMaxBufSize = getPrefs().getInt(Param.TIMESHIFT_DURATION);
 
-		_timeshiftUrl = getPrefs().getString(Param.TIMESHIFT_URL);
-		_timeshiftStartUrl = _timeshiftUrl + getPrefs().getString(Param.TIMESHIFT_START_URL_PARAM);
-		_timeshiftTimeFormat = new SimpleDateFormat(getPrefs().getString(Param.TIME_FORMAT), Locale.getDefault());
-
 		reset();
 
-		final String host = getPrefs().getString(Param.HOST);
-		final int port = getPrefs().getInt(Param.PORT);
-		_networkClient = new NetworkClient(host, port, new NetworkClient.OnNetworkEvent()
-		{
-			@Override
-			public void onDisconnected()
-			{
-			}
-
-			@Override
-			public void onDataReceived(String data)
-			{
-			}
-
-			@Override
-			public void onConnected(boolean success)
-			{
-				if (success)
-				{
-					Log.i(TAG, ".onConnected: " + host + ":" + port);
-				}
-				else
-				{
-					Log.e(TAG, ".onConnected: Unable to connect to nethogs service on " + host + ":" + port);
-				}
-				_timeshiftAvailable = success;
-				FeatureTimeshift.super.initialize(onFeatureInitialized);
-			}
-		});
-
-		_feature.Component.SYSTEM.command("stop timeshifter");
-		_feature.Component.SYSTEM.command("start timeshifter");
-
-		getEventMessenger().postDelayed(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				_networkClient.connect();
-			}
-		}, 1000);
+		super.initialize(onFeatureInitialized);
 	}
-
-	/**
-	 * Play url with timeshift buffer
-	 *
-	 * @param streamUrl
-	 */
-//	public void playJni(final String streamUrl)
-//	{
-//		Log.i(TAG, ".play: " + streamUrl);
-//		reset();
-//
-//		if (!isTimeshiftAvailable())
-//		{
-//			_featurePlayer.play(streamUrl);
-//		}
-//		else
-//		{
-//			if (_featurePlayer.isPlaying())
-//				_featurePlayer.stop();
-//			getEventMessenger().postDelayed(new Runnable()
-//			{
-//				@Override
-//				public void run()
-//				{
-//					new Thread(new Runnable()
-//					{
-//						@Override
-//						public void run()
-//						{
-//							Log.i(TAG, ".play: " + streamUrl);
-//							int exitCode = ffmpeg_start(streamUrl);
-//							Log.i(TAG, "ffmpeg_start exited with code " + exitCode);
-//						}
-//					}).start();
-//
-//					Log.i(TAG, ".play: prepare timeshift");
-//					getEventMessenger().removeCallbacks(_switcher);
-//					getEventMessenger().post(_switcher);
-//				}
-//			}, 1000);
-//		}
-//	}
 
 	/**
 	 * Play url with timeshift buffer
@@ -249,21 +92,8 @@ public class FeatureTimeshift extends FeatureComponent implements EventReceiver
 		Log.i(TAG, ".play: " + streamUrl);
 		reset();
 
-		_networkClient.command(streamUrl);
-		getEventMessenger().postDelayed(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				_feature.Component.PLAYER.play(_timeshiftStartUrl);
-			}
-		}, 2000);
-	}
-
-	public boolean isTimeshiftAvailable()
-	{
-		Log.d(TAG, ".isTimeshiftAvailable: " + _timeshiftAvailable);
-		return _timeshiftAvailable;
+		_streamUrl = streamUrl;
+		_feature.Component.PLAYER.play(streamUrl);
 	}
 
 	/**
