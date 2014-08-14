f281dbe83be9d297b78155cd64745f48aee48973
Alexander Marinov <alek.marinov@aviq.bg>
2014-08-14 11:25:53 +0300

    resisting FeatureChannel on optional dependency of FeatureTimeshift

--- src/com/aviq/tv/android/sdk/feature/register/FeatureRegister.java
+++ src/com/aviq/tv/android/sdk/feature/register/FeatureRegister.java
@@ -53,7 +53,7 @@ public class FeatureRegister extends FeatureComponent
 		/**
 		 * ABMP Server address
 		 */
-		ABMP_SERVER(""),
+		ABMP_SERVER("http://services.aviq.com:978"),
 
 		/**
 		 * Brand name to represent the box on ABMP
