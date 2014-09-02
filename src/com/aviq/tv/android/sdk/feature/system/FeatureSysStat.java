00a0f739cae125dc8dd9f5df42dad608fb6293bd
Elmira Pavlova <elmira.pavlova@aviq.bg>
2014-09-02 17:41:14 +0300

    Feature #197 implementation

---
+++ src/com/aviq/tv/android/sdk/feature/system/FeatureSysStat.java
@@ -0,0 +1,79 @@
+package com.aviq.tv.android.sdk.feature.system;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.ExecutorService;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.aviq.tv.android.sdk.core.EventMessenger;
+import com.aviq.tv.android.sdk.core.feature.FeatureComponent;
+import com.aviq.tv.android.sdk.core.feature.FeatureName;
+import com.aviq.tv.android.sdk.core.feature.FeatureName.Component;
+
+public class FeatureSysStat extends FeatureComponent
+{
+	
+	public static final String TAG = FeatureSysStat.class.getSimpleName();
+	public static final int ON_STATUS = EventMessenger.ID("ON_STATUS");
+	
+	@Override
+	public Component getComponentName()
+	{
+		// TODO Auto-generated method stub
+		return FeatureName.Component.SYSTEM_STAT;
+	}
+	
+	@Override
+	public void initialize(final OnFeatureInitialized onFeatureInitialized)
+	{
+		Log.i(TAG, ".initialize");
+		
+		new Thread(new Runnable()
+		{
+			public void run()
+			{
+				BufferedReader reader = null;
+				try
+				{
+					Process proc = Runtime.getRuntime().exec("vmstat -d 1");
+					reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+					String line = reader.readLine();
+					while (line != null)
+					{
+						String[] pieces = line.split(" ");
+						long freemem = Long.parseLong(pieces[2]);
+						long cpuidle = Long.parseLong(pieces[12]);
+						
+						Bundle bundle = new Bundle();
+						bundle.putLong("cpuidle", cpuidle);
+						bundle.putLong("freemem", freemem);
+						getEventMessenger().trigger(ON_STATUS, bundle);
+						line = reader.readLine();
+					}
+				}
+				catch (IOException error)
+				{
+					Log.e(TAG, error.getMessage());
+					if (reader != null)
+					{
+						try
+						{
+							reader.close();
+						}
+						catch (IOException e)
+						{
+							Log.e(TAG, e.getMessage());
+						}
+					}
+				}
+			}
+		}).start();
+		
+		FeatureSysStat.super.initialize(onFeatureInitialized);
+		
+	}
+	
+}
