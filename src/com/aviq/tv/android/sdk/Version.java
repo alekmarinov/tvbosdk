414464d0aaaa77440c0198dcd908db63a052ceb2
Alexander Marinov <alek.marinov@aviq.bg>
2015-07-17 18:01:36 +0300

    version incremented

--- src/com/aviq/tv/android/sdk/Version.java
+++ src/com/aviq/tv/android/sdk/Version.java
@@ -17,6 +17,6 @@ public class Version
 {
 	public static final int MAJOR = 1;
 	public static final int MINOR = 4;
-	public static final int REVISION = 116;
+	public static final int REVISION = 129;
 	public static final String NAME = MAJOR + "." + MINOR + "." + REVISION;
 }
